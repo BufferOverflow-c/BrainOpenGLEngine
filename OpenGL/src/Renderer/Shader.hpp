e68cbccd688c1c13925c91b51e6e7d1989051ca5
Kenny Higgins <terrestrialabyss@gmail.com>
2025-11-19 05:05:16 -0500

    moved header includes around to make more sense, also fixed lsp

--- OpenGL/src/Renderer/Shader.hpp
+++ OpenGL/src/Renderer/Shader.hpp
@@ -6,37 +6,35 @@
 
 // STD
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
 
 namespace BrainOpenGL {
 class Shader {
 public:
-    // program ID
-    unsigned int ID;
+  // program ID
+  unsigned int ID;
 
-    // constructor reads/builds shader
-    Shader(const char *vertexPath, const char *fragmentPath);
+  // constructor reads/builds shader
+  Shader(const char *vertexPath, const char *fragmentPath);
 
-    // use/activate shader
-    void use();
+  // use/activate shader
+  void use();
 
-    // utility uniform functions
-    void setBool(const std::string &name, bool value) const;
-    void setInt(const std::string &name, int value) const;
-    void setFloat(const std::string &name, float value) const;
-    void setVec2(const std::string &name, const glm::vec2 &value) const;
-    void setVec2(const std::string &name, float x, float y) const;
-    void setVec3(const std::string &name, const glm::vec3 &value) const;
-    void setVec3(const std::string &name, float x, float y, float z) const;
-    void setVec4(const std::string &name, const glm::vec4 &value) const;
-    void setVec4(const std::string &name, float x, float y, float z, float w) const;
-    void setMat2(const std::string &name, const glm::mat2 &mat) const;
-    void setMat3(const std::string &name, const glm::mat3 &mat) const;
-    void setMat4(const std::string &name, const glm::mat4 &mat) const;
+  // utility uniform functions
+  void setBool(const std::string &name, bool value) const;
+  void setInt(const std::string &name, int value) const;
+  void setFloat(const std::string &name, float value) const;
+  void setVec2(const std::string &name, const glm::vec2 &value) const;
+  void setVec2(const std::string &name, float x, float y) const;
+  void setVec3(const std::string &name, const glm::vec3 &value) const;
+  void setVec3(const std::string &name, float x, float y, float z) const;
+  void setVec4(const std::string &name, const glm::vec4 &value) const;
+  void setVec4(const std::string &name, float x, float y, float z,
+               float w) const;
+  void setMat2(const std::string &name, const glm::mat2 &mat) const;
+  void setMat3(const std::string &name, const glm::mat3 &mat) const;
+  void setMat4(const std::string &name, const glm::mat4 &mat) const;
 
 private:
-    void checkCompileErrors(unsigned int shader, std::string type);
+  void checkCompileErrors(unsigned int shader, std::string type);
 };
-}
+} // namespace BrainOpenGL
